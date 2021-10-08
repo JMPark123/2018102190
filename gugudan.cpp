261bd2cc2dd47a0455ff5b616c5c24aa7f1e04e2
junhyuk0801 <junhyuk0801@gmail.com>
2021-10-08 10:35:35 +0900

    changed nine-by-nine

--- gugudan.cpp
+++ gugudan.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main(){
-    for (int i = 1;i <= 8;i++){
+    for (int i = 1;i <= 9;i++){
+        // changed nine-by-nine
         cout << i << "단" << endl;
         for (int j = 1;j <= 9;j++){
             cout << i << " * " << j << " = " << i * j << endl;
