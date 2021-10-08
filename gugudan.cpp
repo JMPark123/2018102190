ba2e6fd5b3f6ad52ef809911d51ac37eadfbc702
hellowhales <init.bhs990310@gmail.com>
2021-10-08 10:27:05 +0900

    gugudan.cpp

--- gugudan.cpp
+++ gugudan.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-    for (int i = 1;i <= 8;i++){
+    for (int i = 1;i <= 9;i++){ // 9·Î ¼ö Á¤  
         cout << i << "ë‹¨" << endl;
         for (int j = 1;j <= 9;j++){
             cout << i << " * " << j << " = " << i * j << endl;
         }
     }
-    return 0;
+    return 0; 
 }
